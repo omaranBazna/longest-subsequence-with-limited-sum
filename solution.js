419a2a550a892aebb236743b2dc7a613427a1d4a
omaranBazna <100542103+omaranBazna@users.noreply.github.com>
2022-08-29 10:19:51 -0400

    Update solution.js
    implement binary search to make the code faster

--- solution.js
+++ solution.js
@@ -2,43 +2,39 @@ var answerQueries = function(nums, queries) {
 nums.sort((a,b)=>a-b)
 let sum=[]
 let acc=0;
-
 for(let el of nums){
     acc +=el;
     sum.push(acc);
-    
 }
-  
     let result=[]
     for(let el of queries){
         result.push(find(sum,el))
     }
     return result
 };
-
-
-
-
 const find=(arr,target)=>{
- let left=0;
+let left=0;
 let right=arr.length-1;
-
-    if(target>arr[right]){
+    if(target>=arr[right]){
         return arr.length
     }
     if(target<arr[left]){
         return 0;
     }
- for(let i=0;i<arr.length;i++){
-
-     if(arr[i+1]){
-     if(target>=arr[i] && target<arr[i+1]){
-         return i+1
-     }
-     }else{
-         if(target>=arr[i]){
-             return i+1
-         }
-     }
- }
+    let mid=Math.floor((right+left)/2)
+while(right>left){
+    if(target<arr[mid]){
+        right=mid-1
+    }else if(target>arr[mid]){
+        left=mid+1
+    }else if(target==arr[mid]){
+        return mid+1
+    }
+      mid=Math.floor((right+left)/2)
+}
+    if(target>arr[mid]){
+        return mid+1
+    }else{
+        return mid
+    }
 }
